ef35dde44b9c2d919c965178cca03a6379366682
bachnxuan <bachnxuan@gmail.com>
2026-02-12 15:58:41 +0700

    refactor(build.sh): remove useless MAKE_OPTS
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- build.sh
+++ build.sh
@@ -17,16 +17,6 @@ CFLAGS="-O2 -DNDEBUG -ffunction-sections -fdata-sections -fno-asynchronous-unwin
 CPPFLAGS="-DZSTD_STRIP_ERROR_STRINGS=1"
 LDFLAGS="-static -Wl,--gc-sections,--strip-all"
 
-MAKE_OPTS=(
-    ZSTD_NOBENCH=1        # Drops benchmark code
-    ZSTD_NODICT=1         # Drops dict training
-    ZSTD_LEGACY_SUPPORT=0 # Drops legacy decode
-    HAVE_THREAD=0         # No threading support
-    HAVE_ZLIB=0           # No gzip support
-    HAVE_LZMA=0           # No xz/lzma support
-    HAVE_LZ4=0            # No lz4 support
-)
-
 prepare() {
     # Prepare directory
     info "Preparing directories..."
@@ -40,14 +30,14 @@ prepare() {
     # Clone source
     info "Cloning zstd..."
     local tag="$(latest_tag facebook/zstd)"
-    git_clone "facebook/zstd@$tag" "$SOURCE" &> /dev/null
+    git_clone "facebook/zstd@$tag" "$SOURCE" &>/dev/null
 }
 
 build() {
     info "Building zstd..."
     make -j"$JOBS" -C "$SOURCE/programs" \
         CC="$CC" CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" LDFLAGS="$LDFLAGS" \
-        "${MAKE_OPTS[@]}" "$TARGET"
+        "$TARGET"
 }
 
 finalize() {
